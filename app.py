70893aaa7987ca95281d4b143b6c83796ba4fd7e
harshit11purohit <harshitpurohit953@gmail.com>
2026-03-24 20:44:51 +0530

    fix: update app.py imports and integrate data ingestion pipeline

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 from src.ML_PROJECT.logger import logging
 from src.ML_PROJECT.exception import CustomException
-
-
+# from src.ML_project import logger
+from src.ML_PROJECT.components.data_ingestion import DataIngestion
+from src.ML_PROJECT.components.data_ingestion import DataIngestionConfig
+import sys
 
 if __name__=="__main__":
     logging.info(" the execution has started")
     
     try:
-        a=1/0
+       data_ingestion=DataIngestion()
+       data_ingestion.initiate_data_ingestion()
     except Exception as e:
         logging.info(" custom exception")
         raise CustomException(e,sys)
-    
+
+
