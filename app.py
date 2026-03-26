3e03d5833429660f4233ba6e41458fa4c2355ddc
harshit11purohit <harshitpurohit953@gmail.com>
2026-03-26 11:30:11 +0530

    feat: integrate model training execution into the main app pipeline

--- app.py
+++ app.py
@@ -1,32 +1,30 @@
 from src.ML_PROJECT.logger import logging
 from src.ML_PROJECT.exception import CustomException
-# from src.ML_project import logger
 from src.ML_PROJECT.components.data_ingestion import DataIngestion
 from src.ML_PROJECT.components.data_ingestion import DataIngestionConfig
 from src.ML_PROJECT.components.data_transformation import DataTransformationConfig,DataTransformation
+from src.ML_PROJECT.components.model_trainer import ModelTrainerConfig,ModelTrainer
+
 import sys
 
+
 if __name__=="__main__":
-    logging.info(" the execution has started")
-    
+    logging.info("The execution has started")
+
     try:
-       data_ingestion=DataIngestion()
-       train_data_path,test_data_path=data_ingestion.initiate_data_ingestion()
-       
-       #data_transformation_config=DataTransformationConfig()
-       data_transformation=DataTransformation()
-       data_transformation.initiate_data_transormation(train_data_path,test_data_path)
-       
-    
-    
-    
-    
-      
-      
-      
-       
+        #data_ingestion_config=DataIngestionConfig()
+        data_ingestion=DataIngestion()
+        train_data_path,test_data_path=data_ingestion.initiate_data_ingestion()
+
+        #data_transformation_config=DataTransformationConfig()
+        data_transformation=DataTransformation()
+        train_arr,test_arr,_=data_transformation.initiate_data_transormation(train_data_path,test_data_path)
+
+        ## Model Training
+
+        model_trainer=ModelTrainer()
+        print(model_trainer.initiate_model_trainer(train_arr,test_arr))
+        
     except Exception as e:
-        logging.info(" custom exception")
+        logging.info("Custom Exception")
         raise CustomException(e,sys)
-
-
