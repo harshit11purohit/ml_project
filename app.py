cd52fef3b6ac2997dcaa32bee42130f72828d4b0
harshit11purohit <harshitpurohit953@gmail.com>
2026-03-27 20:56:16 +0530

    feat: integrate ingestion, transformation, and training components into main execution script

--- app.py
+++ app.py
@@ -18,10 +18,11 @@ if __name__=="__main__":
 
         #data_transformation_config=DataTransformationConfig()
         data_transformation=DataTransformation()
-        train_arr,test_arr,_=data_transformation.initiate_data_transormation(train_data_path,test_data_path)
+        #train_arr,test_arr,_=data_transformation.initiate_data_transormation(train_data_path,test_data_path)
+        train_arr, test_arr, preprocessor_path = data_transformation.initiate_data_transformation(train_data_path, test_data_path)
 
-        ## Model Training
-
+        print(type(train_arr))
+        print(type(test_arr))
         model_trainer=ModelTrainer()
         print(model_trainer.initiate_model_trainer(train_arr,test_arr))
         
